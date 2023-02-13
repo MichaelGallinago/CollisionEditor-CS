5a80ea74b3edd88721d3b41e46e4f24bd309922c
Michael Gallinago <michael.gallinago@gmail.com>
2023-02-13 15:42:02 +0500

    Git fixed
    ura pobeda

---
+++ CollisionEditor/model/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionEditor.model
+{
+    class Anglemap
+    {
+        public List<byte> values { get; set; }
+
+        public Anglemap(string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException();
+
+            BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open));
+            values = reader.ReadBytes(int.MaxValue).ToList();
+        }
+    }
+
+    class TileStrip
+    {
+        public List<Bitmap> bitmaps { get; set; }
+
+        public TileStrip(string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException();
+
+            Bitmap bitmap = new Bitmap(fileName);
+            int count = (bitmap.Width / 16) * (bitmap.Height / 16);
+            for (int i = 0; i < count; i++)
+            {
+
+            }
+            bitmap.GetPixel(0, 0);
+            //bitmaps.Add();
+        }
+    }
+}
