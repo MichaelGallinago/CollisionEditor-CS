92c9ee0f251aa2c336b660707611510577210449
Michael Gallinago <michael.gallinago@gmail.com>
2023-02-15 10:43:41 +0500

    Load tiles

--- CollisionEditor/model/Class1.cs
+++ CollisionEditor/model/Class1.cs
@@ -11,33 +11,69 @@ namespace CollisionEditor.model
 {
     class Anglemap
     {
-        public List<byte> values { get; set; }
+        public List<byte> Values { get; set; }
 
-        public Anglemap(string fileName)
+        public Anglemap(string path)
         {
+            BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+            Values = reader.ReadBytes((int)reader.BaseStream.Length).ToList();
+        }
 
-            BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open));
-            values = reader.ReadBytes((int)reader.BaseStream.Length).ToList();
+        public void Save(string path)
+        {
+            if (File.Exists(path)) 
+                File.Delete(path);
+
+            BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.CreateNew));
+            writer.Write(Values.ToArray());
         }
     }
 
     class TileStrip
     {
+        public readonly int TileWidth;
+        public readonly int TileHeight;
+
         public List<Bitmap> bitmaps { get; set; }
 
-        public TileStrip(string fileName)
+        public TileStrip(string path, int tileWidth = 16, int tileHeight = 16, 
+            int separateX = 0, int separateY = 0, int offsetX = 0, int offsetY = 0)
         {
-            if (!File.Exists(fileName))
-                throw new FileNotFoundException();
+            TileWidth  = tileWidth;
+            TileHeight = tileHeight;
 
-            Bitmap bitmap = new Bitmap(fileName);
-            int count = (bitmap.Width / 16) * (bitmap.Height / 16);
-            for (int i = 0; i < count; i++)
+            bitmaps = new List<Bitmap>();
+            Bitmap bitmap = new Bitmap(path);
+
+            int rowCount    = (bitmap.Width  - offsetX) / tileWidth;
+            int columnCount = (bitmap.Height - offsetY) / tileHeight;
+            for (int y = 0; y < columnCount; y++)
+            {
+                for (int x = 0; x < rowCount; x++)
+                {
+                    Rectangle tile = new Rectangle(x * (tileWidth + separateX) + offsetX, 
+                        y * (tileHeight + separateY) + offsetY, TileWidth, TileHeight);
+                    bitmaps.Add(bitmap.Clone(tile, bitmap.PixelFormat));
+                }
+            }
+        }
+
+        public void Save(string path, int rowCount, int tileWidth = 16, int tileHeight = 16,
+            int separateX = 0, int separateY = 0, int offsetX = 0, int offsetY = 0)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+
+            int width  = offsetX + rowCount * (TileWidth + separateX) - separateX;
+            int height = offsetX + rowCount * (TileWidth + separateX) - separateX;
+            Bitmap image = new Bitmap(width, height);
+
+            foreach (Bitmap bitmap in bitmaps)
             {
 
             }
-            bitmap.GetPixel(0, 0);
-            //bitmaps.Add();
+
+            image.Save(path, ImageFormat.Png);
         }
     }
 }
