8b3aabd0ca8225e0d0a7b31bb362b3802c0d5947
informant <artemiy.solodkin@mail.ru>
2023-02-15 17:38:22 +0500

    NEWBITMAP

--- CollisionEditor/MainWindow.xaml.cs
+++ CollisionEditor/MainWindow.xaml.cs
@@ -3,6 +3,10 @@ using System.Windows;
 using System.Windows.Forms;
 using CollisionEditor.model;
 using static CollisionEditor.MainWindow;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Windows.Media;
+
 namespace CollisionEditor
 {
     /// <summary>
@@ -23,7 +27,24 @@ namespace CollisionEditor
                 filePath = openFileDialog.FileName;  
             }
             Anglemap anglemap = new Anglemap(filePath);
-            System.Windows.Forms.MessageBox.Show(string.Join(" ",anglemap.values));
+            System.Windows.Forms.MessageBox.Show(string.Join(" ",anglemap.Values));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int width = 128;
+            int height = width;
+            int stride = width / 8;
+            byte[] pixels = new byte[height * stride];
+
+            // Try creating a new image with a custom palette.
+            List<System.Windows.Media.Color> colors = new List<System.Windows.Media.Color>();
+            colors.Add(System.Windows.Media.Colors.Red);
+            colors.Add(System.Windows.Media.Colors.Blue);
+            colors.Add(System.Windows.Media.Colors.Green);
+            BitmapPalette myPalette = new BitmapPalette(colors);
+
+            ImageOfTile.Source = BitmapSource.Create(width,height,96,96,PixelFormats.Indexed1,myPalette,pixels,stride);
         }
     }
 }
