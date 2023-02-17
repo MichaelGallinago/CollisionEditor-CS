4594ea7d08084474e03b386ebddd20c0d8bc5a17
informant <artemiy.solodkin@mail.ru>
2023-02-17 22:31:43 +0500

    Сетку, фильтр и сохранение сделал

--- CollisionEditor/MainWindow.xaml.cs
+++ CollisionEditor/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using static CollisionEditor.MainWindow;
 using System.Windows.Media.Imaging;
 using System.Collections.Generic;
 using System.Windows.Media;
+using System.IO;
 
 namespace CollisionEditor
 {
@@ -14,32 +15,58 @@ namespace CollisionEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Tilemap tileStrip = null;
         public MainWindow()
         {
             
         }
         private void MenuOpenAngleMapClick(object sender, RoutedEventArgs e)
         {
+            
+
             System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
             string filePath = string.Empty;
+            openFileDialog.Filter = "Binary Files(*.bin)| *.bin| All files(*.*) | *.*";
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                filePath = openFileDialog.FileName;  
+                ImageOfTile.Source = null;
+                filePath = openFileDialog.FileName;
+                Anglemap anglemap = new Anglemap(filePath);
+                System.Windows.Forms.MessageBox.Show(string.Join(" ", anglemap.Values));
             }
-            Anglemap anglemap = new Anglemap(filePath);
-            System.Windows.Forms.MessageBox.Show(string.Join(" ",anglemap.Values));
         }
 
         private void MenuOpenTileStripClick(object sender, RoutedEventArgs e)
         {
+            
+            
             System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.png)| *.png| All files(*.*) | *.*";
             string filePath = string.Empty;
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+                ImageOfTile.Source = null;
                 filePath = openFileDialog.FileName;
+                tileStrip = new Tilemap(filePath);
+                ImageOfTile.Source = Convertor.BitmapConvert(tileStrip.bitmaps[5]);
             }
-            Tilemap tileStrip = new Tilemap(filePath);
-            ImageOfTile.Source = Convertor.Convert(tileStrip.bitmaps[5]);
+        }
+
+        private void MenuSaveTiletmapClick(object sender, RoutedEventArgs e)
+        {
+            if (tileStrip is null)
+            {
+                System.Windows.Forms.MessageBox.Show("Ошибка: Вы не выбрали Tilemap, чтобы её сохранить");
+            }
+            
+            System.Windows.Forms.SaveFileDialog saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            
+            saveFileDialog.Filter = "Image Files(*.png)| *.png";
+            //Спрашивать RowCount
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tileStrip.Save(Path.GetFullPath(saveFileDialog.FileName), 1);
+            }
         }
     }
 }
