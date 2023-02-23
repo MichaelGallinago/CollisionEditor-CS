9a1a0700a9f922ab0e3217daecb3946fee1fa020
Michael Gallinago <michael.gallinago@gmail.com>
2023-02-23 17:12:55 +0500

    Position correction

---
+++ CollisionEditor/model/AngleConstructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionEditor.model
+{
+    internal class AngleConstructor
+    {
+        const int cellSize = 8;
+        public Vector2<int> GetCorrectDotPosition(Vector2<double> position)
+        {
+            return new Vector2<int>(
+                (int)Math.Round(position.X) & -cellSize, 
+                (int)Math.Round(position.Y) & -cellSize);
+        }
+    }
+}
