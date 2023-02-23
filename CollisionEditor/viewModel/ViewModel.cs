94149cfe18e85b69deaade98cb2f38249cba2cb8
informant <artemiy.solodkin@mail.ru>
2023-02-23 11:57:48 +0500

    LastUpdate

--- CollisionEditor/viewModel/ViewModel.cs
+++ CollisionEditor/viewModel/ViewModel.cs
@@ -1,13 +1,6 @@
 ﻿using CollisionEditor.model;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CollisionEditor;
 using System.IO;
-using System.Reflection.Metadata;
 
 namespace CollisionEditor.viewModel
 {
