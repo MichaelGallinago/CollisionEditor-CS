2e7f99243a88b3b3f4b369693155dda050e6452f
informant <artemiy.solodkin@mail.ru>
2023-02-23 17:37:46 +0500

    ggg

--- CollisionEditor/model/AngleConstructor.cs
+++ CollisionEditor/model/AngleConstructor.cs
@@ -5,12 +5,8 @@ namespace CollisionEditor.model
     static internal class AngleConstructor
     {
         const int cellSize = 8;
-<<<<<<< HEAD
+        
         public static Vector2<int> GetCorrectDotPosition(Vector2<double> position)
-=======
-
-        public Vector2<int> GetCorrectDotPosition(Vector2<double> position)
->>>>>>> develop
         {
             return new Vector2<int>(
                 (int)Math.Round(position.X) & -cellSize, 
