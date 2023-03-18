e8753bdfa3300a080f3a43e986416185f947d00d
informant <artemiy.solodkin@mail.ru>
2023-03-18 22:12:26 +0500

    Help

--- CollisionEditor/MainWindow.xaml.cs
+++ CollisionEditor/MainWindow.xaml.cs
@@ -182,7 +182,7 @@ namespace CollisionEditor
         {
             Process.Start(new ProcessStartInfo()
             {
-                FileName = "https://youtu.be/m_3w_qcbcXs?t=21",
+                FileName = "https://youtu.be/m5sbRiwQPMQ?t=86",
                 UseShellExecute = true,
             });
         }
